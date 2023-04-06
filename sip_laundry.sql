6c131a0bc3fd51f4b2f1a7d45efa2fd3ee3bc6ad
Azza Wafiqurrohmah <wafiqurrohmahazza@gmail.com>
2023-04-07 01:47:12 +0700

    add field DB

--- sip_laundry.sql
+++ sip_laundry.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Mar 23, 2023 at 05:23 AM
--- Server version: 10.6.12-MariaDB-0ubuntu0.22.04.1
--- PHP Version: 8.1.2-1ubuntu2.11
+-- Host: 127.0.0.1
+-- Generation Time: Apr 06, 2023 at 08:45 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -31,7 +31,17 @@ CREATE TABLE `customers` (
   `customer_id` int(11) NOT NULL,
   `name` varchar(150) NOT NULL,
   `phone` varchar(15) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `customers`
+--
+
+INSERT INTO `customers` (`customer_id`, `name`, `phone`) VALUES
+(36, 'sasa', '087546534211'),
+(37, 'sinta', '087546534211'),
+(38, 'sisi', '087546534211'),
+(39, 'nana', '087546534211');
 
 -- --------------------------------------------------------
 
@@ -43,8 +53,19 @@ CREATE TABLE `laundries` (
   `laundry_id` int(11) NOT NULL,
   `unit` enum('kilogram','meter','pcs') NOT NULL,
   `cost` int(11) NOT NULL,
-  `name` varchar(150) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `name` varchar(150) NOT NULL,
+  `IsExpress` tinyint(1) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `laundries`
+--
+
+INSERT INTO `laundries` (`laundry_id`, `unit`, `cost`, `name`, `IsExpress`) VALUES
+(29, 'meter', 3000, 'korden', NULL),
+(30, 'pcs', 7000, 'boneka', NULL),
+(31, 'kilogram', 5000, 'baju', NULL),
+(32, 'meter', 4000, 'karpet', NULL);
 
 -- --------------------------------------------------------
 
@@ -56,7 +77,7 @@ CREATE TABLE `options` (
   `option_id` int(11) NOT NULL,
   `key` varchar(100) NOT NULL,
   `value` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -67,13 +88,24 @@ CREATE TABLE `options` (
 CREATE TABLE `transactions` (
   `transaction_id` int(11) NOT NULL,
   `transaction_date` datetime NOT NULL,
+  `ritard` int(3) DEFAULT 0,
   `pickup_date` datetime NOT NULL,
-  `status` enum('process','finish','taken') NOT NULL,
+  `status` enum('process','finish','taken') DEFAULT 'process',
   `payment_status` enum('paid','unpaid') NOT NULL,
   `amount` int(11) NOT NULL DEFAULT 0,
   `user_id` int(11) NOT NULL,
   `customer_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `transactions`
+--
+
+INSERT INTO `transactions` (`transaction_id`, `transaction_date`, `ritard`, `pickup_date`, `status`, `payment_status`, `amount`, `user_id`, `customer_id`) VALUES
+(22, '2023-04-03 13:09:45', NULL, '2023-04-08 13:09:45', 'finish', 'paid', 18000, 8, 36),
+(23, '2023-04-01 13:14:43', NULL, '2023-04-03 13:14:43', 'finish', 'unpaid', 14000, 19, 38),
+(24, '2023-04-02 13:16:48', NULL, '2023-04-02 13:16:48', 'finish', 'unpaid', 8000, 20, 37),
+(25, '2023-04-02 13:23:13', NULL, '2023-04-02 13:23:13', 'taken', 'unpaid', 20000, 20, 39);
 
 -- --------------------------------------------------------
 
@@ -86,7 +118,18 @@ CREATE TABLE `transaction_details` (
   `subtotal` int(11) NOT NULL DEFAULT 0,
   `transaction_id` int(11) NOT NULL,
   `laundry_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `transaction_details`
+--
+
+INSERT INTO `transaction_details` (`qty`, `subtotal`, `transaction_id`, `laundry_id`) VALUES
+(1, 3000, 22, 29),
+(3, 15000, 22, 31),
+(2, 14000, 23, 30),
+(2, 8000, 24, 32),
+(4, 20000, 25, 31);
 
 --
 -- Triggers `transaction_details`
@@ -117,7 +160,21 @@ CREATE TABLE `users` (
   `password` varchar(100) NOT NULL,
   `address` mediumtext NOT NULL,
   `role` enum('admin','cashier') NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `fullname`, `phone`, `password`, `address`, `role`) VALUES
+(8, 'aldea', '3232333333', 'haikamu', 'jember', 'cashier'),
+(19, 'sinta', '089787675654', 'kamu', 'jember', 'admin'),
+(20, 'sinta', '089787675654', 'kamu', 'jember', 'admin'),
+(21, 'sinta', '089787675654', 'kamu', 'jember', 'admin'),
+(26, 'sinta', '089787675654', 'kamu', 'jember', 'admin'),
+(31, 'sinta', '089787675654', 'kamu', 'jember', 'admin'),
+(37, 'aldea', '3232333333', 'haikamu', 'jember', 'cashier'),
+(38, 'sinta', '089787675654', 'kamu', 'jember', 'admin');
 
 -- --------------------------------------------------------
 
@@ -128,7 +185,7 @@ CREATE TABLE `users` (
 CREATE TABLE `verifications` (
   `user_id` int(11) NOT NULL,
   `code` varchar(8) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Indexes for dumped tables
@@ -187,37 +244,37 @@ ALTER TABLE `verifications`
 -- AUTO_INCREMENT for table `customers`
 --
 ALTER TABLE `customers`
-  MODIFY `customer_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `customer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
 
 --
 -- AUTO_INCREMENT for table `laundries`
 --
 ALTER TABLE `laundries`
-  MODIFY `laundry_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `laundry_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
 
 --
 -- AUTO_INCREMENT for table `options`
 --
 ALTER TABLE `options`
-  MODIFY `option_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `option_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `transactions`
 --
 ALTER TABLE `transactions`
-  MODIFY `transaction_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `transaction_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
 
 --
 -- AUTO_INCREMENT for table `verifications`
 --
 ALTER TABLE `verifications`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
 
 --
 -- Constraints for dumped tables
