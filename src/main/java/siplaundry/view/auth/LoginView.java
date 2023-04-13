a937a557accd103f1a586f53a5fb2a9e28cfc8d8
irsyadulibad <ahmadirsyadulibad7@gmail.com>
2023-04-13 15:51:27 +0700

    add(view): Login page

--- src/main/java/siplaundry/view/auth/LoginView.java
+++ src/main/java/siplaundry/view/auth/LoginView.java
@@ -1,6 +1,11 @@
 package siplaundry.view.auth;
 
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class LoginView extends Application {
@@ -9,7 +14,15 @@ public class LoginView extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/pages/auth/login.fxml"));
+        Scene scene = new Scene(root);
 
+        scene.getStylesheets().add(getClass().getResource("/style/main.css").toString());
+        scene.getStylesheets().add(getClass().getResource("/style/auth/login.css").toString());
+
+        primaryStage.setTitle("Login - SIP Laundry");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
