8f585b89d24f6004905aa52c8b9edbd97798bfbd
Azza Wafiqurrohmah <wafiqurrohmahazza@gmail.com>
2023-04-20 23:44:25 +0700

    add barcode code

---
+++ src/main/java/siplaundry/service/BarcodeService.java
@@ -0,0 +1,15 @@
+package siplaundry.service;
+
+// import com.google.zxing.oned.EAN13Writer;
+
+public class BarcodeService {
+
+    public String setIdBarcode(String date, int id){
+        String newId = Integer.toString(id);
+        return date + "-" + newId;
+    }
+
+    private void generate(String IdBarcode){
+        
+    }
+}
