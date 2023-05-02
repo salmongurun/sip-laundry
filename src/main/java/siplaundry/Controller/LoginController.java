dab9a0cc3ba0f37931397e1c30087bd64fbf8def
Azza Wafiqurrohmah <wafiqurrohmahazza@gmail.com>
2023-05-02 12:54:09 +0700

    add tray notification, and controller

---
+++ src/main/java/siplaundry/Controller/LoginController.java
@@ -0,0 +1,51 @@
+package siplaundry.Controller;
+
+import javafx.animation.Animation;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.util.Duration;
+import siplaundry.service.AuthService;
+import siplaundry.tray.animations.AnimationType;
+import siplaundry.tray.notification.NotificationType;
+import siplaundry.tray.notification.TrayNotification;
+
+public class LoginController {
+
+    @FXML
+    private Button BtnLogin;
+    @FXML
+    private TextField TxtUserName;
+    @FXML
+    private TextField TxtPassword;
+
+    public void ButtonLoginAction(ActionEvent ex){
+        System.out.println("Ping");
+        String title = "sign in";
+        String message1 = "username atau password yang anda masukkan salah";
+        String message2 = "username atau password yang anda masukkan benar";
+
+        TrayNotification tray = new TrayNotification();
+        AnimationType type = AnimationType.POPUP;
+        
+         if(!new AuthService().login(TxtUserName.getText(), TxtPassword.getText())){
+             tray.setAnimationType(type);
+             tray.setTitle(title);
+             tray.setMessage(message1);
+
+             tray.setNotificationType(NotificationType.WARNING);
+             tray.showAndDismiss(Duration.millis(1000));
+             return;
+         }
+
+        tray.setAnimationType(type);
+        tray.setTitle(title);
+        tray.setMessage(message2);
+
+        tray.setNotificationType(NotificationType.SUCCESS);
+        tray.showAndDismiss(Duration.millis(3000));
+    }
+
+}
---
+++ src/main/java/siplaundry/tray/notification/NotificationType.java
@@ -0,0 +1,10 @@
+package siplaundry.tray.notification;
+
+public enum NotificationType {
+    INFORMATION,
+    NOTICE,
+    SUCCESS,
+    WARNING,
+    ERROR,
+    CUSTOM
+}
