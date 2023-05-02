58491cea29f3ff7bfb8f73645481bec2c65b0927
Azza Wafiqurrohmah <wafiqurrohmahazza@gmail.com>
2023-05-02 13:45:36 +0700

    change Controller to controller, and move adminViewController to controller

--- src/main/java/siplaundry/Controller/AdminViewController.java
+++ src/main/java/siplaundry/Controller/AdminViewController.java
@@ -1,4 +1,4 @@
-package siplaundry.view;
+package siplaundry.controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.input.MouseEvent;
--- src/main/java/siplaundry/Controller/LoginController.java
+++ src/main/java/siplaundry/Controller/LoginController.java
@@ -1,9 +1,7 @@
-package siplaundry.Controller;
+package siplaundry.controller;
 
-import javafx.animation.Animation;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.util.Duration;
