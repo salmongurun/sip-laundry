71204ec60d9069fe3727ef06783fa54f3f81216b
irsyadulibad <ahmadirsyadulibad7@gmail.com>
2023-05-17 06:19:14 +0700

    chore: update account ui

--- src/main/java/siplaundry/entity/WhatsappMessage.java
+++ src/main/java/siplaundry/entity/WhatsappMessage.java
@@ -16,4 +16,20 @@ public class WhatsappMessage {
         this.target = target;
         this.text = text;
     }
+
+    public String getTarget() {
+        return target;
+    }
+
+    public void setTarget(String target) {
+        this.target = target;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
 }
