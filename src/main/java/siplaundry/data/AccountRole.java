079b52c4679b577f9d861a420337258a77be8b6d
irsyadulibad <ahmadirsyadulibad7@gmail.com>
2023-05-17 08:13:08 +0700

    fix: account role string not equals to db

--- src/main/java/siplaundry/data/AccountRole.java
+++ src/main/java/siplaundry/data/AccountRole.java
@@ -2,12 +2,5 @@ package siplaundry.data;
 
 public enum AccountRole {
     admin,
-    cashier,
-    ;
-
-    @Override
-    public String toString() {
-        if(this == admin) return "Admin";
-        return "Kasir";
-    }
+    cashier;
 }
