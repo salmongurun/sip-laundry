0087ef5b161d22544f41cd8d6014816a2b9adef3
Azza Wafiqurrohmah <wafiqurrohmahazza@gmail.com>
2023-05-24 08:52:18 +0700

    add show and hide password in login

--- src/main/java/siplaundry/controller/LoginController.java
+++ src/main/java/siplaundry/controller/LoginController.java
@@ -4,9 +4,11 @@ import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 import siplaundry.service.AuthService;
@@ -18,24 +20,34 @@ import tray.notification.TrayNotification;
 
 import java.io.IOException;
 
+import org.kordamp.ikonli.javafx.FontIcon;
+
 public class LoginController {
 
     @FXML
     private Button BtnLogin;
     @FXML
-    private TextField TxtUserName, TxtPassword;
+    private TextField TxtUserName;
     @FXML
     private BorderPane shadowRoot;
+    @FXML
+    private HBox password_container;
+    @FXML
+    private FontIcon toggle_icon;
+
+    private String password;
+    private boolean isPasswordShown = false;
 
     public void initialize() {
         shadowRoot.setVisible(false);
+        addPasswordElement();
     }
 
     public void ButtonLoginAction() throws IOException {
         Stage stage = (Stage) TxtUserName.getScene().getWindow();
         TrayNotification tray = new TrayNotification();
         
-         if(!new AuthService().login(TxtUserName.getText(), TxtPassword.getText())){
+         if(!new AuthService().login(TxtUserName.getText(), password)){
             tray.setTray("sign in","username atau password yang anda masukkan salah", NotificationType.WARNING, AnimationType.POPUP);
             tray.showAndDismiss(Duration.millis(500));
              return;
@@ -60,4 +72,45 @@ public class LoginController {
         new VerificationView(shadowRoot);
     }
 
+    @FXML
+    void togglePassword() {
+        password_container.getChildren().remove(0);
+        
+        if(isPasswordShown) {
+            toggle_icon.setIconLiteral("bx-show");
+            addPasswordElement();
+        } else {
+            toggle_icon.setIconLiteral("bx-hide");
+            addVisiblePasswordElement();
+        }
+
+        isPasswordShown = !isPasswordShown;
+
+    }
+
+    private void addPasswordElement() {
+        PasswordField field = new PasswordField();
+        field.getStyleClass().add("input");
+        field.setPrefSize(419, 45);
+        field.setText(password);
+
+        field.setOnKeyReleased(event -> {
+            this.password = field.getText();
+        });
+        
+        password_container.getChildren().add(0, field);
+    }
+
+    private void addVisiblePasswordElement() {
+        TextField field = new TextField();
+        field.getStyleClass().add("input");
+        field.setPrefSize(419, 45);
+        field.setText(password);
+
+        field.setOnKeyReleased(event -> {
+            this.password = field.getText();
+        });
+
+        password_container.getChildren().add(0, field);
+    }
 }
