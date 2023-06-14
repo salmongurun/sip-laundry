4922f485d54e70ab9731908a8bc37c8b71d03939
Azza Wafiqurrohmah <wafiqurrohmahazza@gmail.com>
2023-06-14 11:25:44 +0700

    add transaction table cashier

--- src/main/java/siplaundry/controller/cashier/TransactionController.java
+++ src/main/java/siplaundry/controller/cashier/TransactionController.java
@@ -1,23 +1,160 @@
 package siplaundry.controller.cashier;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import siplaundry.data.SessionData;
+import siplaundry.entity.TransactionEntity;
+import siplaundry.repository.TransactionRepo;
+import siplaundry.view.admin.components.modal.ConfirmDialog;
 import siplaundry.view.cashier.TransactionFormView;
+import siplaundry.view.cashier.components.column.TransactionColumn;
+import toast.Toast;
+import toast.ToastType;
 
 public class TransactionController {
     private BorderPane parent_root, shadow_root;
+
+    @FXML
+    private HBox btn_bulk_delete;
+
+    @FXML
+    private VBox trans_table;
+
+    @FXML
+    private Text total_text;
+
+    @FXML
+    private TextField txt_keyword;
+
+    @FXML
+    private ComboBox<String> CB_sortBy, CB_column;
+
+    private TransactionRepo transRepo = new TransactionRepo();
+    private Set<TransactionEntity> bulkItems = new HashSet<>();
+    private ArrayList<TransactionColumn> accColumns = new ArrayList<>();
+
     public TransactionController(BorderPane parent_root, BorderPane shadow_root) {
         this.parent_root = parent_root;
         this.shadow_root = shadow_root;
     }
 
     @FXML
-    void initialize() {
+    void initialize(){
+        ObservableList<String> items = FXCollections.observableArrayList(
+            "A-Z",
+            "Z-A"
+        );
+        CB_sortBy.setItems(items);
 
+        ObservableList<String> column = FXCollections.observableArrayList(
+            "Nama Pelanggan",
+            "Tanggal Transaksi"
+        );
+        CB_column.setItems(column);
+
+        List<TransactionEntity> trans = transRepo.get();
+        showTable(trans);
     }
+
+    @FXML
+    void SortAction(){
+        String SortBy = " DESC";
+        String column = " pickup_date";
+
+        if(CB_sortBy.getValue().equals("A-Z")) SortBy = " ASC";
+        if(CB_column.getValue().equals("Nama Pelanggan")) column = " customers.name";
+
+        List<TransactionEntity> trans = transRepo.sortTable(
+            "customers.name", 
+            " JOIN customers ON transactions.customer_id = customers.customer_id", 
+            column, 
+            SortBy
+        );
+
+        showTable(trans);
+    }
+
+    @FXML
+    void searchAction(KeyEvent event){
+        String keyword = txt_keyword.getText();
+
+        List<TransactionEntity> trans = transRepo.searchTable(
+            "customers.name", 
+            " JOIN customers ON transactions.customer_id = customers.customer_id", 
+            new HashMap<>(){{
+                put("cuatomers.name", keyword);
+                put("transacctions.payment_status", keyword);
+            }}
+        );
+        showTable(trans);
+    }
+
+    @FXML
+    void selectBulkAll(){
+        for(TransactionColumn column: accColumns){
+            column.toggleBulk();
+        }
+    }
+
+    @FXML
+    void bulkDelete(){
+        new ConfirmDialog(shadow_root, () -> {
+            for(TransactionEntity trans: this.bulkItems){
+                transRepo.delete(trans.getid());
+            }
+
+            new Toast((AnchorPane) btn_bulk_delete.getScene().getRoot())
+                    .show(ToastType.SUCCESS, "Berhasil melakukan hapus semua", null);
+            showTable(transRepo.get());
+        });
+    }
+
+    public void showTable(List<TransactionEntity> trans){
+        accColumns.clear();
+        trans_table.getChildren().clear();
+
+        if(trans == null) trans = transRepo.get();
+        for(TransactionEntity transEn: trans){
+                if(!(transEn.getUserID().getID().equals(SessionData.user.getID()))) continue;
+
+                TransactionColumn column = new TransactionColumn(shadow_root, this::showTable, transEn);
+                column.setBulkAction(this::toggleBulkItem);
+
+                trans_table.getChildren().add(column);
+                accColumns.add(column);
+            
+        }
+        total_text.setText("Menampilkan total " + trans.size() + " data akun");
+    }
+
+    protected void toggleBulkItem(TransactionEntity trans){
+        if(this.bulkItems.contains(trans)){
+            this.bulkItems.remove(trans);
+        } else {
+            this.bulkItems.add(trans);
+        }
+
+        btn_bulk_delete.setDisable(this.bulkItems.size() < 1);
+    }
+
     @FXML
     void showAddTransaction() {
         this.parent_root.setCenter(new TransactionFormView(parent_root, shadow_root));
     }
+
 }
---
+++ src/main/java/siplaundry/view/cashier/components/column/TransactionColumn.java
@@ -0,0 +1,115 @@
+package siplaundry.view.cashier.components.column;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.function.Consumer;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.CheckBox;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
+import siplaundry.data.LaundryStatus;
+import siplaundry.data.PaymentStatus;
+import siplaundry.entity.TransactionEntity;
+import siplaundry.repository.TransactionRepo;
+import siplaundry.view.admin.components.modal.ConfirmDialog;
+import toast.Toast;
+import toast.ToastType;
+
+public class TransactionColumn extends HBox {
+    @FXML
+    private Text txt_date, txt_custName, txt_amount;
+
+    @FXML
+    private Text txt_status, txt_payment;
+
+    @FXML
+    private HBox payment_background, status_background, edit_btn, delete_btn;
+
+    @FXML
+    private CheckBox bulk_Check;
+
+    private String payment = "Belum Lunas";
+    private String status = "Prosess";
+
+    private TransactionEntity trans;
+    private TransactionRepo transRepo = new TransactionRepo();
+    private BorderPane shadowRoot;
+    SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/YYYY");
+    private Consumer<List<TransactionEntity>> refreshTable;
+
+    public TransactionColumn(BorderPane shadowRoot, Consumer<List<TransactionEntity>> refreshTable, TransactionEntity trans){
+        this.trans = trans;
+        this.shadowRoot = shadowRoot;
+        this.refreshTable = refreshTable;
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/pages/cashier/transaction/column.fxml"));
+
+        loader.setRoot(this);
+        loader.setController(this);
+
+        try { loader.load(); } 
+        catch(IOException e) { e.printStackTrace(); }
+    }
+
+    @FXML
+    void initialize(){
+        setPaymentColor();
+        setStatusColor();
+
+        txt_date.setText(dateFormat.format(trans.gettransactionDate()));
+        txt_custName.setText(trans.getCustomerID().getname());
+        txt_amount.setText(String.valueOf(trans.getamount()));
+        txt_payment.setText(payment);
+        txt_status.setText(status);
+
+        delete_btn.setOnMouseClicked(event -> {
+            new ConfirmDialog(shadowRoot, () -> {
+                transRepo.delete(trans.getid());
+                
+                new Toast((AnchorPane) shadowRoot.getScene().getRoot())
+                    .show(ToastType.SUCCESS, "Berhasil menghapus Data Transaksi", null);
+                refreshTable.accept(null);
+            });
+        });
+
+    }
+
+    public void setBulkAction(Consumer<TransactionEntity> action) {
+        bulk_Check.selectedProperty().addListener((ob, ov, nv) -> {
+            action.accept(this.trans);
+        });
+    }
+    public void toggleBulk(){
+        bulk_Check.setSelected(!bulk_Check.isSelected());
+    }
+
+
+    void setPaymentColor(){
+        if(trans.getPaymentStatus() == PaymentStatus.instalment){
+            payment = "Cicilan";
+            txt_payment.setStyle("-fx-fill: #feb74d;");
+            payment_background.setStyle(payment_background.getStyle() + "-fx-background-color: #fff4e5;");
+        }else if(trans.getPaymentStatus() == PaymentStatus.paid){
+            payment = "Lunas";
+            txt_payment.setStyle("-fx-fill: #6A9A98;");
+            payment_background.setStyle(payment_background.getStyle() + "-fx-background-color: #f0f4f4;");
+        }
+    }
+
+    void setStatusColor(){
+        if(trans.getstatus() == LaundryStatus.finish){
+            status = "Selesai";
+            txt_status.setStyle("-fx-fill: #feb74d;");
+            status_background.setStyle(status_background.getStyle() + "-fx-background-color: #fff4e5;" );
+        } else if(trans.getstatus() == LaundryStatus.taken){
+            status = "Diambil";
+            txt_status.setStyle("-fx-fill: #6A9A98;");
+            status_background.setStyle(status_background.getStyle() + "-fx-background-color: #f0f4f4;");
+        }
+    }
+}
