f9d61aadc741c06d07bb5b4afabacbc20f12bcc1
irsyadulibad <ahmadirsyadulibad7@gmail.com>
2023-06-18 16:59:11 +0700

    feat: add transaction detail

--- src/main/java/siplaundry/controller/invoice/InvoiceDetailController.java
+++ src/main/java/siplaundry/controller/invoice/InvoiceDetailController.java
@@ -3,18 +3,31 @@ package siplaundry.controller.invoice;
 import javafx.fxml.FXML;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
+import siplaundry.entity.TransactionDetailEntity;
 import siplaundry.entity.TransactionEntity;
+import siplaundry.repository.TransactionDetailRepo;
 import siplaundry.util.NumberUtil;
+import siplaundry.util.TransactionUtil;
 import siplaundry.util.ViewUtil;
+import siplaundry.view.admin.TransactionView;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
 
 public class InvoiceDetailController {
     @FXML
-    private Text transaction_name, customer_name, cashier_name, transaction_date, due_date, total_text, status_text, payment_text;
+    private Text transaction_name, customer_name, cashier_name, transaction_date, due_date, total_text, status_text, payment_text, retard_text;
     @FXML
-    private HBox payment_background, status_background;
+    private HBox payment_background, status_background, retard_element;
+    @FXML
+    private VBox detail_container;
+
     private BorderPane parent_root, shadow_root;
     private TransactionEntity transaction;
+    private TransactionDetailRepo detailRepo = new TransactionDetailRepo();
 
     public InvoiceDetailController(BorderPane parentRoot, BorderPane shadowRoot, TransactionEntity transaction) {
         this.parent_root = parentRoot;
@@ -24,15 +37,35 @@ public class InvoiceDetailController {
 
     @FXML
     void initialize() {
-        transaction_name.setText("Pesanan#" + transaction.getid());
+        transaction_name.setText("Transaksi#" + transaction.getid());
         customer_name.setText(transaction.getCustomerID().getname());
         cashier_name.setText(transaction.getUserID().getFullname());
         total_text.setText("Rp " + NumberUtil.rupiahFormat(transaction.getamount()));
         transaction_date.setText(ViewUtil.formatDate(transaction.gettransactionDate(), "dd/MM/YYYY"));
         due_date.setText(ViewUtil.formatDate(transaction.getpickupDate(), "dd/MM/YYYY"));
+        retard_text.setText("Tertunda " + transaction.getRetard() + " Hari");
+
+
+        if(transaction.getRetard() < 1) retard_element.setVisible(false);
 
         paymentStatusColor();
         setTransactionColor();
+        addDetailElements();
+    }
+
+    @FXML
+    void backAction() throws IOException {
+        parent_root.setCenter(new TransactionView(parent_root, shadow_root));
+    }
+
+    void addDetailElements() {
+        List<TransactionDetailEntity> details = detailRepo.get(new HashMap<>() {{
+            put("transaction_id", transaction.getid());
+        }});
+
+        for(TransactionDetailEntity detail: details) {
+            detail_container.getChildren().add(TransactionUtil.generateDetails(detail));
+        }
     }
 
     void paymentStatusColor() {
--- src/main/java/siplaundry/util/TransactionUtil.java
+++ src/main/java/siplaundry/util/TransactionUtil.java
@@ -1,9 +1,14 @@
 package siplaundry.util;
 
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import siplaundry.entity.LaundryEntity;
 import siplaundry.entity.TransactionDetailEntity;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class TransactionUtil {
     public static int getExistLaundry(LaundryEntity laundry, ArrayList<TransactionDetailEntity> details) {
@@ -17,5 +22,29 @@ public class TransactionUtil {
         }
 
         return launIndex;
+    }
+
+    public static HBox generateDetails(TransactionDetailEntity detail) {
+        HBox container = new HBox();
+        HBox itemContainer = new HBox();
+
+        Text detailName = new Text(detail.getLaundry().getname());
+        Text detailQty = new Text("x" + String.valueOf(detail.getQty()) + " " + detail.getLaundry().getunit());
+        Text detailSubtotal = new Text("Rp " + NumberUtil.rupiahFormat(detail.getSubtotal()));
+
+        itemContainer.setPrefWidth(200);
+        itemContainer.setSpacing(10);
+
+        container.getStyleClass().add("invoice-detail");
+        detailName.getStyleClass().add("name");
+        detailQty.getStyleClass().add("qty");
+
+        detailSubtotal.setWrappingWidth(250);
+        detailSubtotal.setTextAlignment(TextAlignment.RIGHT);
+
+        itemContainer.getChildren().addAll(detailName, detailQty);
+        container.getChildren().addAll(itemContainer, detailSubtotal);
+
+        return container;
     }
 }
