53d62a60dd8e0d8dbd7db6ee5365f0db3eb73b51
Azza Wafiqurrohmah <wafiqurrohmahazza@gmail.com>
2023-06-19 16:37:39 +0700

    add addRfid in account

---
+++ src/main/java/siplaundry/controller/admin/account/AddRfidController.java
@@ -0,0 +1,25 @@
+package siplaundry.controller.admin.account;
+
+import javafx.fxml.FXML;
+import javafx.stage.Stage;
+
+public class AddRfidController {
+    private Stage primaryStage;
+
+    public AddRfidController(Stage primaryStage){ this.primaryStage = primaryStage;}
+
+    @FXML
+    void initialize() throws InterruptedException{
+
+    }
+
+    @FXML
+    void checkRFID(){
+
+    }
+
+    @FXML
+    void closeModal(){
+
+    }
+}
