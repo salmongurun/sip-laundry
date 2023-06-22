817a7b1dd2694def1fb305e95eabcb8765cda15b
salmongurun <rofiqimuhammad437@gmail.com>
2023-06-22 09:04:25 +0700

    Update App.java

--- src/main/java/siplaundry/App.java
+++ src/main/java/siplaundry/App.java
@@ -9,7 +9,7 @@ import siplaundry.view.auth.LoginView;
 public class App {
     public static void main(String[] args) {
          Timer timer = new Timer();
-         TimerTask task = new TimerTask() {
+         TimerTask task = new TimerTask() { 
              @Override
              public void run() {
                 StatusService coba = new StatusService();
